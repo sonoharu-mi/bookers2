fe15b5b326fa4f94fbf7876ca54cb99e018a227f
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-08-11 05:15:29 +0000

    profile_image表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
   
   def index
     @book = Book.new
-    @books = @user.books
+    @users = User.all
+    @books = Book.all
   end
 
   def show
     @user = User.find(params[:id])
     @book_new = Book.new
-    @books = @user.books
+    @books = @user.books.page(params[:page])
   end
 
   def edit
