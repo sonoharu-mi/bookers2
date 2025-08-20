66af146d95c5a734a667562862213d4bea2ff69b
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-08-20 12:45:31 +0000

    text_clear

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -57,7 +57,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     unless @user.id == current_user.id
-      redirect_to user_path(current_user.id)
+      redirect_to books_path
     end
   end
 end
